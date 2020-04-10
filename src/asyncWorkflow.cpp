a20bcbdda1a75ea0c43b15b2271b8d6ec26d963f
TimGuite <tim.guite@blueyonder.co.uk>
2020-04-10 16:54:11 +0100

    Added async executable
    Almost identical to syncCli, but with sleeps on the threads. Might add this to sync version too. Highlights that tasks are taking place at the same time

---
+++ src/asyncWorkflow.cpp
@@ -0,0 +1,88 @@
+/*
+Example of a workflow being executed in a synchronous manner, with CLI input
+to provide permission for manual processes
+*/
+
+#include <iostream>
+#include <locale>
+#include <string>
+#include <thread>
+
+#include "asyncExecutor.hpp"
+#include "cursor.hpp"
+#include "step.hpp"
+#include "termcolor.hpp"
+#include "workflow.hpp"
+
+using namespace cursor;
+using namespace asyncexecutor;
+using namespace step;
+using namespace workflow;
+using namespace std;
+
+bool cliSeekPermission(const Step &s) {
+  string result;
+  string in;
+  while (result != "y" && result != "n") {
+    cout << "**************" << endl;
+    cout << "Step " << s.id << " seeking permission to run." << endl;
+    cout << "Description: " << s.description << endl;
+    cout << "Permission provided? (Y/N)" << endl;
+    cin >> in;
+    result = tolower(in[0]);
+  }
+
+  return (result == "y");
+}
+
+void onUpdate(cursor::Cursor &c) {
+  // Print out information about the state of the workflow
+  cout << "================" << endl;
+  auto state = c.viewState();
+  for (auto step : state) {
+    cout << step.first << ": ";
+    switch (step.second) {
+    case waiting:
+      cout << termcolor::white << "waiting";
+      break;
+    case ready:
+      cout << termcolor::on_yellow << "ready";
+      break;
+    case fail:
+      cout << termcolor::on_red << "failed";
+      break;
+    case complete:
+      cout << termcolor::on_green << "complete";
+      break;
+    }
+    cout << termcolor::reset << endl;
+  }
+}
+
+// Empty function implementation with sleep
+void f() { std::this_thread::sleep_for(5s); }
+
+int main() {
+
+  const Workflow w =
+      makeWorkflow({{"A", "Add Reagent 1", f, manual, {}},
+                    {"B", "Add Reagent 2", f, manual, {"A"}},
+                    {"C", "Preheat Heater", f, automatic, {"B"}},
+                    {"D", "Mix Reagents", f, automatic, {"B"}},
+                    {"E", "Heat Sample", f, automatic, {"C", "D"}},
+                    {"F", "Extract Sample", f, manual, {"E"}}});
+
+  Cursor c{w};
+
+  auto res = asyncExecutor(c, cliSeekPermission, onUpdate);
+
+  if (res == success) {
+    cout << termcolor::on_green << "Execution completed successfully."
+         << termcolor::reset << endl;
+  } else {
+    cout << termcolor::on_red << "Execution failed." << termcolor::reset
+         << endl;
+  }
+
+  return 0;
+}
