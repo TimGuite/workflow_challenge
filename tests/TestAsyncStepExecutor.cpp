05fda98dc8377051661c8e500099f247d6555d5d
TimGuite <tim.guite@blueyonder.co.uk>
2020-04-10 16:00:48 +0100

    Added further tests for async step executor

--- tests/TestAsyncStepExecutor.cpp
+++ tests/TestAsyncStepExecutor.cpp
@@ -40,7 +40,82 @@ TEST_CASE("Asynchronous Step Executor", "[executor]") {
       unique_lock lk(queueM);
       queueCV.wait(lk);
       REQUIRE(queue.size() == 1);
+      REQUIRE(queue.front().first == "a");
       REQUIRE(queue.front().second == success);
+      // Indicates future has finished
+      REQUIRE(f1.valid() == true);
+    }
+  }
+
+  SECTION("Two asynchronous functions") {
+    mutex ioM;
+    mutex queueM;
+    condition_variable queueCV;
+    list<pair<string, ExecutionResult>> queue;
+    Step s1{"a", "Step a", successfulTaskAsync, automatic, {}};
+    Step s2{"b", "Step b", successfulTaskAsync, automatic, {}};
+
+    auto f1 = async(launch::async, asyncStepExecutor, ref(ioM), ref(queueM),
+                    ref(queueCV), ref(queue), ref(s1),
+                    successfulPermissionRequestAsync);
+    auto f2 = async(launch::async, asyncStepExecutor, ref(ioM), ref(queueM),
+                    ref(queueCV), ref(queue), ref(s2),
+                    successfulPermissionRequestAsync);
+
+    // Check that a successful result came back on the queue
+    {
+      unique_lock lk(queueM);
+      // Wait twice
+      queueCV.wait(lk);
+      // Can't be guaranteed that both steps will run first
+      if (queue.size() == 1) {
+        queueCV.wait(lk);
+      }
+      // Establish the values are different
+      REQUIRE(queue.front().first != queue.back().first);
+      // Check the results
+      REQUIRE(queue.front().second == success);
+      REQUIRE(queue.back().second == success);
+      // Indicates future has finished
+      REQUIRE(f1.valid() == true);
+      REQUIRE(f2.valid() == true);
+    }
+  }
+
+  SECTION("One success, one failure") {
+    mutex ioM;
+    mutex queueM;
+    condition_variable queueCV;
+    list<pair<string, ExecutionResult>> queue;
+    Step s1{"a", "Step a", successfulTaskAsync, automatic, {}};
+    Step s2{"b", "Step b", problematicTaskAsync, automatic, {}};
+
+    auto f1 = async(launch::async, asyncStepExecutor, ref(ioM), ref(queueM),
+                    ref(queueCV), ref(queue), ref(s1),
+                    successfulPermissionRequestAsync);
+    auto f2 = async(launch::async, asyncStepExecutor, ref(ioM), ref(queueM),
+                    ref(queueCV), ref(queue), ref(s2),
+                    successfulPermissionRequestAsync);
+
+    // Check that a successful result came back on the queue
+    {
+      unique_lock lk(queueM);
+      // Wait twice
+      queueCV.wait(lk);
+      // Can't be guaranteed that both steps will run first
+      if (queue.size() == 1) {
+        queueCV.wait(lk);
+      }
+      // Establish the values are different
+      REQUIRE(queue.front().first != queue.back().first);
+      // Check the results
+      if (queue.front().first == "a") {
+        REQUIRE(queue.front().second == success);
+        REQUIRE(queue.back().second == failure);
+      } else {
+        REQUIRE(queue.back().second == success);
+        REQUIRE(queue.front().second == failure);
+      }
     }
   }
 }
