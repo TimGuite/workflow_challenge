e49205cb9b2d5c77dddcde5abc6dfba1b316e737
TimGuite <tim.guite@blueyonder.co.uk>
2020-04-10 15:11:00 +0100

    Added async step executor
    This will take a step (and a number of other parameters) and run the step's task, returning an ExecutionResult in a message queue.
    
    By separating out this execution step we can keep the cursor in the main thread only.
    
    I was originally planning to keep this in the .cpp file only, as it is an implementation detail, but clearly there is a need to test it and make sure it is working how I think it is working. Therefore, a definition has been added to the header file and I will create some unit tests for the functionality.

--- src/executor.cpp
+++ src/executor.cpp
@@ -1,5 +1,8 @@
+#include <condition_variable>
 #include <functional>
+#include <mutex>
 #include <string>
+#include <thread>
 #include <utility>
 #include <vector>
 
@@ -54,6 +57,69 @@ blockingExecutor(Cursor &cur,
   }
 
   // Check if all of the steps are completed
+  bool all_complete = cur.completedSteps().size() == cur.flow.size();
+
+  return (all_complete ? success : failure);
+}
+
+// Function to asynchronously execute steps and push the result on to the queue
+void asyncStepExecutor(mutex &ioMutex, mutex &queueMutex,
+                       condition_variable &queueCV,
+                       list<pair<string, ExecutionResult>> &messageQueue,
+                       Step &s,
+                       function<bool(const Step &)> permissionRequest) {
+
+  ExecutionResult res;
+
+  try {
+    // Run the task in the first ready step
+    if (s.type == automatic) {
+      s.task();
+      res = success;
+    } else if (s.type == manual) {
+      // Mutex scope
+      {
+        // Acquire I/O mutex
+        std::scoped_lock<std::mutex> ioLock(ioMutex);
+        // Seek permission
+        bool permission = permissionRequest(s);
+        // I/O lock released
+      }
+      if (permission) {
+        // Execute if given
+        s.task();
+        res = success;
+      } else {
+        // Mark as failed if not
+        res = failure;
+      }
+    }
+  }
+  // Expect most functions to throw an exception with a message
+  catch (char e) {
+    res = failure;
+  }
+  // Also need to catch any exception which is thrown
+  catch (...) {
+    res = failure;
+  }
+
+  // Mutex scope
+  {
+    // Acquire the queue mutex
+    std::scoped_lock<std::mutex> queueLock(queueMutex);
+    // Return the id and a result
+    messageQueue.push_back({s.id, res});
+    // Queue mutex released
+  }
+  // Notify the queueCV
+  queueCV.notify_one();
+}
+
+ExecutionResult
+asyncExecutor(Cursor &cur, function<bool(const step::Step &)> permissionRequest,
+              function<void(Cursor &)> onUpdate) {
+
   bool all_complete = cur.completedSteps().size() == cur.flow.size();
 
   return (all_complete ? success : failure);
--- src/executor.hpp
+++ src/executor.hpp
@@ -1,6 +1,11 @@
+#include <condition_variable>
 #include <functional>
+#include <list>
+#include <mutex>
+#include <utility>
 
 #include "cursor.hpp"
+#include "step.hpp"
 
 #ifndef EXECUTOR_H_
 #define EXECUTOR_H_
@@ -9,11 +14,17 @@ namespace executor {
 
 enum ExecutionResult { success, failure };
 
-ExecutionResult blockingExecutor(cursor::Cursor &, std::function<bool(const step::Step &)>,
+ExecutionResult blockingExecutor(cursor::Cursor &,
+                                 std::function<bool(const step::Step &)>,
                                  std::function<void(cursor::Cursor &)>);
 
-ExecutionResult asyncExecutor(cursor::Cursor &, std::function<bool(const step::Step &)>,
-                                 std::function<void(cursor::Cursor &)>);
+ExecutionResult asyncExecutor(cursor::Cursor &,
+                              std::function<bool(const step::Step &)>,
+                              std::function<void(cursor::Cursor &)>);
+
+void asyncStepExecutor(std::mutex &, std::mutex &, std::condition_variable &,
+                       std::list<pair<std::string, ExecutionResult>> &,
+                       step::Step &, function<bool(const step::Step &)>)
 
 } // namespace executor
 
